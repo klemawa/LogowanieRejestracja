54941d3c7bfd25dfdc80a1cc9c070763cb9e7a0e
klemawa <116188369+klemawa@users.noreply.github.com>
2024-03-31 14:37:11 +0200

    Dodaj pliki projektów.

---
+++ LoginReg/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace LoginReg
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxLoginLogin = new System.Windows.Forms.TextBox();
+            this.textBoxPasswordLogin = new System.Windows.Forms.TextBox();
+            this.textBoxPasswordLoginRepeat = new System.Windows.Forms.TextBox();
+            this.textBoxLoginRegister = new System.Windows.Forms.TextBox();
+            this.textBoxPasswordRegister = new System.Windows.Forms.TextBox();
+            this.textBoxPasswordRegisterRepeat = new System.Windows.Forms.TextBox();
+            this.buttonLogin = new System.Windows.Forms.Button();
+            this.buttonRegister = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxLoginLogin
+            // 
+            this.textBoxLoginLogin.Location = new System.Drawing.Point(59, 70);
+            this.textBoxLoginLogin.Name = "textBoxLoginLogin";
+            this.textBoxLoginLogin.Size = new System.Drawing.Size(100, 20);
+            this.textBoxLoginLogin.TabIndex = 0;
+            this.textBoxLoginLogin.Text = "login";
+            // 
+            // textBoxPasswordLogin
+            // 
+            this.textBoxPasswordLogin.Location = new System.Drawing.Point(59, 128);
+            this.textBoxPasswordLogin.Name = "textBoxPasswordLogin";
+            this.textBoxPasswordLogin.Size = new System.Drawing.Size(100, 20);
+            this.textBoxPasswordLogin.TabIndex = 1;
+            this.textBoxPasswordLogin.Text = "password";
+            // 
+            // textBoxPasswordLoginRepeat
+            // 
+            this.textBoxPasswordLoginRepeat.Location = new System.Drawing.Point(59, 154);
+            this.textBoxPasswordLoginRepeat.Name = "textBoxPasswordLoginRepeat";
+            this.textBoxPasswordLoginRepeat.Size = new System.Drawing.Size(100, 20);
+            this.textBoxPasswordLoginRepeat.TabIndex = 2;
+            this.textBoxPasswordLoginRepeat.Text = "repeat password";
+            // 
+            // textBoxLoginRegister
+            // 
+            this.textBoxLoginRegister.Location = new System.Drawing.Point(256, 70);
+            this.textBoxLoginRegister.Name = "textBoxLoginRegister";
+            this.textBoxLoginRegister.Size = new System.Drawing.Size(100, 20);
+            this.textBoxLoginRegister.TabIndex = 3;
+            this.textBoxLoginRegister.Text = "login";
+            // 
+            // textBoxPasswordRegister
+            // 
+            this.textBoxPasswordRegister.Location = new System.Drawing.Point(256, 128);
+            this.textBoxPasswordRegister.Name = "textBoxPasswordRegister";
+            this.textBoxPasswordRegister.Size = new System.Drawing.Size(100, 20);
+            this.textBoxPasswordRegister.TabIndex = 4;
+            this.textBoxPasswordRegister.Text = "password";
+            // 
+            // textBoxPasswordRegisterRepeat
+            // 
+            this.textBoxPasswordRegisterRepeat.BackColor = System.Drawing.SystemColors.Window;
+            this.textBoxPasswordRegisterRepeat.Location = new System.Drawing.Point(256, 154);
+            this.textBoxPasswordRegisterRepeat.Name = "textBoxPasswordRegisterRepeat";
+            this.textBoxPasswordRegisterRepeat.Size = new System.Drawing.Size(100, 20);
+            this.textBoxPasswordRegisterRepeat.TabIndex = 5;
+            this.textBoxPasswordRegisterRepeat.Text = "repeat password";
+            // 
+            // buttonLogin
+            // 
+            this.buttonLogin.Location = new System.Drawing.Point(69, 200);
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.Size = new System.Drawing.Size(75, 23);
+            this.buttonLogin.TabIndex = 6;
+            this.buttonLogin.Text = "Zaloguj";
+            this.buttonLogin.UseVisualStyleBackColor = true;
+            this.buttonLogin.Click += new System.EventHandler(this.buttonLogin_Click);
+            // 
+            // buttonRegister
+            // 
+            this.buttonRegister.Location = new System.Drawing.Point(265, 200);
+            this.buttonRegister.Name = "buttonRegister";
+            this.buttonRegister.Size = new System.Drawing.Size(75, 23);
+            this.buttonRegister.TabIndex = 7;
+            this.buttonRegister.Text = "Zarejstruj sie!";
+            this.buttonRegister.UseVisualStyleBackColor = true;
+            this.buttonRegister.Click += new System.EventHandler(this.buttonRegister_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(75, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Logowanie";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(280, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Rejestracja";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(413, 287);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonRegister);
+            this.Controls.Add(this.buttonLogin);
+            this.Controls.Add(this.textBoxPasswordRegisterRepeat);
+            this.Controls.Add(this.textBoxPasswordRegister);
+            this.Controls.Add(this.textBoxLoginRegister);
+            this.Controls.Add(this.textBoxPasswordLoginRepeat);
+            this.Controls.Add(this.textBoxPasswordLogin);
+            this.Controls.Add(this.textBoxLoginLogin);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonLogin;
+        private System.Windows.Forms.Button buttonRegister;
+        private System.Windows.Forms.TextBox textBoxLoginLogin;
+        private System.Windows.Forms.TextBox textBoxPasswordLogin;
+        private System.Windows.Forms.TextBox textBoxPasswordLoginRepeat;
+        private System.Windows.Forms.TextBox textBoxLoginRegister;
+        private System.Windows.Forms.TextBox textBoxPasswordRegister;
+        private System.Windows.Forms.TextBox textBoxPasswordRegisterRepeat;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ LoginReg/Form1.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Data;
+using System.Data.SQLite;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows.Forms;
+
+namespace LoginReg
+{
+    public partial class Form1 : Form
+    {
+        private SQLiteConnection connection;
+        private string dbFileName = "database.db";
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeDatabase();
+        }
+
+        private void InitializeDatabase()
+        {
+            string dbFilePath = Path.Combine(Application.StartupPath, dbFileName);
+
+            // kopiowanie pliku bazy danych do katalogu roboczego, jeśli nie ma go 
+            if (!File.Exists(dbFilePath))
+            {
+                try
+                {
+                    File.Copy(Path.Combine(Application.StartupPath, "database.db"), dbFilePath);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Błąd podczas kopiowania bazy danych: {ex.Message}");
+                    return;
+                }
+            }
+
+            // połączenie z bazą danych 
+            connection = new SQLiteConnection($"Data Source={dbFilePath};Version=3;");
+            connection.Open();
+
+            string createUserTableQuery = "CREATE TABLE IF NOT EXISTS Users (Id INTEGER PRIMARY KEY AUTOINCREMENT, Login TEXT, Password TEXT)";
+            ExecuteQuery(createUserTableQuery);
+        }
+
+        private void ExecuteQuery(string query)
+        {
+            SQLiteCommand cmd = new SQLiteCommand(query, connection);
+            cmd.ExecuteNonQuery();
+        }
+
+        private void buttonRegister_Click(object sender, EventArgs e)
+        {
+            string login = textBoxLoginRegister.Text;
+            string password = textBoxPasswordRegister.Text;
+            string repeatPassword = textBoxPasswordRegisterRepeat.Text;
+
+            if (!ValidateRegistration(login, password, repeatPassword))
+            {
+                return;
+            }
+
+            string checkExistingUserQuery = "SELECT COUNT(*) FROM Users WHERE Login = @Login";
+            using (SQLiteCommand command = new SQLiteCommand(checkExistingUserQuery, connection))
+            {
+                command.Parameters.AddWithValue("@Login", login);
+                int existingUserCount = Convert.ToInt32(command.ExecuteScalar());
+                if (existingUserCount > 0)
+                {
+                    MessageBox.Show("Podany login jest już zajęty. Wybierz inny login.");
+                    return;
+                }
+            }
+
+            // sol
+            byte[] saltBytes = GenerateSalt();
+
+            // hashowabnie hasła z sola
+            byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
+            byte[] saltedPasswordBytes = new byte[passwordBytes.Length + saltBytes.Length];
+            Array.Copy(passwordBytes, saltedPasswordBytes, passwordBytes.Length);
+            Array.Copy(saltBytes, 0, saltedPasswordBytes, passwordBytes.Length, saltBytes.Length);
+            string passwordHash = HashPassword(saltedPasswordBytes);
+
+            // do bazy zapis
+            string insertUserQuery = $"INSERT INTO Users (Login, PasswordHash, Salt) VALUES (@Login, @PasswordHash, @Salt)";
+            try
+            {
+                using (SQLiteCommand cmd = new SQLiteCommand(insertUserQuery, connection))
+                {
+                    cmd.Parameters.AddWithValue("@Login", login);
+                    cmd.Parameters.AddWithValue("@PasswordHash", passwordHash);
+                    cmd.Parameters.AddWithValue("@Salt", Convert.ToBase64String(saltBytes));
+                    cmd.ExecuteNonQuery();
+                }
+                MessageBox.Show("Rejestracja zakończona pomyślnie.");
+            }
+            catch (SQLiteException ex)
+            {
+                MessageBox.Show($"Błąd podczas wstawiania danych użytkownika: {ex.Message}");
+            }
+
+        }
+        private byte[] GenerateSalt()
+        {
+            byte[] salt = new byte[16]; 
+            using (var rng = new RNGCryptoServiceProvider())
+            {
+                rng.GetBytes(salt);
+            }
+            return salt;
+        }
+
+        private string HashPassword(byte[] passwordBytes)
+        {
+            using (var sha256 = SHA256.Create())
+            {
+                byte[] hashBytes = sha256.ComputeHash(passwordBytes);
+                return Convert.ToBase64String(hashBytes);
+            }
+        }
+        private bool VerifyPassword(string login, string password)
+        {
+            string selectUserQuery = "SELECT PasswordHash, Salt FROM Users WHERE Login = @Login";
+            using (SQLiteCommand command = new SQLiteCommand(selectUserQuery, connection))
+            {
+                command.Parameters.AddWithValue("@Login", login);
+                using (SQLiteDataReader reader = command.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        string storedHash = reader.GetString(0);
+                        string storedSalt = reader.GetString(1);
+                        byte[] saltBytes = Convert.FromBase64String(storedSalt);
+
+                        // hashowanie hasła
+                        byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
+                        byte[] saltedPasswordBytes = new byte[passwordBytes.Length + saltBytes.Length];
+                        Array.Copy(passwordBytes, saltedPasswordBytes, passwordBytes.Length);
+                        Array.Copy(saltBytes, 0, saltedPasswordBytes, passwordBytes.Length, saltBytes.Length);
+                        string inputHash = HashPassword(saltedPasswordBytes);
+
+                        return inputHash == storedHash;
+                    }
+                }
+            }
+            return false; // jakby nie było użytkownika takiego
+        }
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+            string login = textBoxLoginLogin.Text;
+            string password = textBoxPasswordLogin.Text;
+
+            // walidacja danych
+            if (string.IsNullOrWhiteSpace(login) || string.IsNullOrWhiteSpace(password))
+            {
+                MessageBox.Show("Wszystkie pola są wymagane.");
+                return;
+            }
+
+            // pobranie hasha i soli od użytkownika z bazy
+            string selectUserQuery = "SELECT PasswordHash, Salt FROM Users WHERE Login = @Login";
+            using (SQLiteCommand command = new SQLiteCommand(selectUserQuery, connection))
+            {
+                command.Parameters.AddWithValue("@Login", login);
+                using (SQLiteDataReader reader = command.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        string storedHash = reader.GetString(0);
+                        string storedSalt = reader.GetString(1);
+                        byte[] saltBytes = Convert.FromBase64String(storedSalt);
+
+                        //hashowanie hasła wprowadzonego przez użytkownika z pobraną solą
+                        byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
+                        byte[] saltedPasswordBytes = new byte[passwordBytes.Length + saltBytes.Length];
+                        Array.Copy(passwordBytes, saltedPasswordBytes, passwordBytes.Length);
+                        Array.Copy(saltBytes, 0, saltedPasswordBytes, passwordBytes.Length, saltBytes.Length);
+                        string inputHash = HashPassword(saltedPasswordBytes);
+
+                        // prównanie hasha wprowadzonego hasła z hashem z bazy danych
+                        if (inputHash == storedHash)
+                        {
+                            MessageBox.Show("Zalogowano pomyślnie!");
+                            return;
+                        }
+                    }
+                }
+            }
+
+            MessageBox.Show("Nieprawidłowy login lub hasło. Spróbuj ponownie.");
+        }
+
+        private bool ValidateRegistration(string login, string password, string repeatedPassword)
+        {
+            // sprawdzanie czy pola nie są puste
+            if (string.IsNullOrWhiteSpace(login) || string.IsNullOrWhiteSpace(password) || string.IsNullOrWhiteSpace(repeatedPassword))
+            {
+                MessageBox.Show("Wszystkie pola są wymagane.");
+                return false;
+            }
+
+            // czy hasła identyczne
+            if (password != repeatedPassword)
+            {
+                MessageBox.Show("Hasła nie pasują do siebie.");
+                return false;
+            }
+
+            // żeby hasło miałoo 6 znaków min
+            if (password.Length < 6)
+            {
+                MessageBox.Show("Hasło powinno zawierać co najmniej 6 znaków.");
+                return false;
+            }
+            return true;
+        }
+        private bool ValidateLogin(string login, string password, string repeatedPassword)
+        {
+            if (string.IsNullOrWhiteSpace(login) || string.IsNullOrWhiteSpace(password) || string.IsNullOrWhiteSpace(repeatedPassword))
+            {
+                MessageBox.Show("Wszystkie pola są wymagane.");
+                return false;
+            }
+
+            if (password != repeatedPassword)
+            {
+                MessageBox.Show("Hasła nie pasują do siebie.");
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
